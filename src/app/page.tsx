3abe4f1908e99a4ea9df026eec2e600ad80eaf12
API2(云盘) <164052842+long2005a1@users.noreply.github.com>
2025-07-13 10:28:10 +0000

    更新 API 接口，统一获取网站配置的参数为 theme，在多个组件中调整数据解构方式，提升代码一致性和可读性。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ interface Props {
 export default async (props: Props) => {
   const searchParams = await props.searchParams;
   const page = searchParams.page || 1;
-  const { data: { value: data } } = (await getWebConfigDataAPI<{ value: Theme }>("layout")) || { data: { value: {} as Theme } };
+  const { data: { value: data } } = (await getWebConfigDataAPI<{ value: Theme }>("theme")) || { data: { value: {} as Theme } };
 
   return (
     <>
